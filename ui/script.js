0e2a0aaf69cb652799c520b5095dab26beef282d
Silantro <128998831+SilantroX@users.noreply.github.com>
2023-11-15 06:31:16 -0500

    Update script.js

--- ui/script.js
+++ ui/script.js
@@ -8,18 +8,13 @@ function createNotification(type, message, iconClass) {
     <div class="progress-bar"></div>
   `;
   container.appendChild(notification);
-
   const icon = notification.querySelector('i');
   const progressBar = notification.querySelector('.progress-bar');
-
-  // Agrega una clase de animación al icono
   icon.classList.add('icon-show');
-
   setTimeout(() => {
     notification.classList.add('show');
     progressBar.style.animation = 'progress-animation 3s linear forwards';
   }, 100);
-
   setTimeout(() => {
     notification.classList.remove('show');
     setTimeout(() => {
@@ -29,15 +24,11 @@ function createNotification(type, message, iconClass) {
 }
 
   
-  // Agregar un listener para el evento 'message'
+  
   window.addEventListener('message', (event) => {
     const data = event.data;
-  
-    // Verificar si el mensaje proviene de sendNUIMessage y tiene el formato esperado
     if (data && data.type && data.message && data.emoji) {
       const { type, message, emoji } = data;
-      
-      // Crear la notificación con el tipo y mensaje recibidos
       createNotification(type, message, emoji);
     }
   });
